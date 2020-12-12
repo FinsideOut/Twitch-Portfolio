0c7574b0b8b4be88bf013079b72d07dcec81aea3
finsignificant <74297078+finsignificant@users.noreply.github.com>
2020-12-12 15:53:53 +0000

    scroll effects header and footer

--- dist/JS/app.js
+++ dist/JS/app.js
@@ -1,1 +1,28 @@
+// scroll stuff
 const header = document.getElementById("header-home");
+const footer = document.getElementById("footer-home");
+window.onscroll = function () {
+  if (window.pageYOffset > 10) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+    footer.classList.remove("footer-scrolled");
+  }
+  if (window.pageYOffset > 200) {
+    footer.classList.add("footer-scrolled");
+  }
+};
+
+// $("a.footer-link").on("click", function (e) {
+//   if (this.hash !== "") {
+//     console.log(e.target);
+//     e.preventDefault();
+//     const hash = this.hash;
+//     $("html, body").animate(
+//       {
+//         scrollTop: $(hash).offset().top,
+//       },
+//       800
+//     );
+//   }
+// });
