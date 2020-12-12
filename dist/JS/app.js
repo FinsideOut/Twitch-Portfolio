7313b404d4298bf81ad3d753a995c84be2f71450
finsignificant <74297078+finsignificant@users.noreply.github.com>
2020-12-12 18:37:48 +0000

    checkpoint 12/12/2020

--- dist/JS/app.js
+++ dist/JS/app.js
@@ -1,28 +1,36 @@
 // scroll stuff
 const header = document.getElementById("header-home");
 const footer = document.getElementById("footer-home");
+const animation = document.getElementById("animation-wrapper");
 window.onscroll = function () {
   if (window.pageYOffset > 10) {
     header.classList.add("header-scrolled");
   } else {
     header.classList.remove("header-scrolled");
     footer.classList.remove("footer-scrolled");
+    animation.classList.add("scrolled-home");
+    animation.classList.remove("scrolled-about");
   }
-  if (window.pageYOffset > 200) {
+  if (window.pageYOffset > 10) {
     footer.classList.add("footer-scrolled");
+  }
+  if (window.pageYOffset > 400) {
+    animation.classList.remove("scrolled-home");
+
+    animation.classList.add("scrolled-about");
   }
 };
 
-// $("a.footer-link").on("click", function (e) {
-//   if (this.hash !== "") {
-//     console.log(e.target);
-//     e.preventDefault();
-//     const hash = this.hash;
-//     $("html, body").animate(
-//       {
-//         scrollTop: $(hash).offset().top,
-//       },
-//       800
-//     );
-//   }
-// });
+$("a").on("click", function (e) {
+  if (this.hash !== "") {
+    console.log(e.target);
+    e.preventDefault();
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100,
+      },
+      800
+    );
+  }
+});
